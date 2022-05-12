a2f552f15519b9a44e16bdc63b83f83b593bf44c
galtoubul <galtoubul@gmail.com>
2022-05-12 12:27:23 +0300

    the timer is now counting down, stopping and it is possible to edit time

--- index.js
+++ index.js
@@ -5,11 +5,34 @@ class Timer {
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
   }
 
   start = () => {
-    
+    // cut the 1 second delay that occurs because setInterval waits its interval before calling its callback
+    this.tick();
+    this.interval = setInterval(this.tick, 1000); // call tick every 1 second
   };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
 }
 
 const durationInput = document.querySelector("#duration");
@@ -17,4 +40,3 @@ const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
 const timer = new Timer(durationInput, startButton, pauseButton);
-timer.start();
